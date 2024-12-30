e43e2a21b6218d9a851a54c23195f78f5108f170
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2024-12-30 16:33:35 +0800

    Create EighteenDragonSubduingPalm7.java

---
+++ src/main/java/basicmod/cards/attack/EighteenDragonSubduingPalm7.java
@@ -0,0 +1,44 @@
+package basicmod.cards.attack;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import basicmod.util.CardStats;
+
+public class EighteenDragonSubduingPalm7 extends AbstractEighteenDragonSubduingPalm {
+    public static final String ID = makeID(EighteenDragonSubduingPalm7.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(CardColor.RED, CardType.ATTACK, CardRarity.BASIC,
+        CardTarget.ENEMY, 1);
+
+    private static final int DAMAGE = 7;
+
+    private static final int BLOCK = 2;
+
+    public EighteenDragonSubduingPalm7() {
+        super(ID, info, 7);
+        setDamage(DAMAGE, 0);
+        setBlock(BLOCK, BLOCK);
+        this.exhaust = true;
+        this.isRunPrePalm = true;
+    }
+
+    @Override
+    protected void runPlam(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        addToBot(new DamageAction(abstractMonster, new DamageInfo(abstractPlayer, damage, DamageInfo.DamageType.NORMAL),
+            AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        this.addToBot(new GainBlockAction(abstractPlayer, abstractPlayer, this.block));
+
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new EighteenDragonSubduingPalm7();
+    }
+
+}
