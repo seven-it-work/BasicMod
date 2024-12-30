38d60e5b7102559735e7f73b3412fc4bed545374
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2024-12-30 16:31:52 +0800

    Create AbstractEighteenDragonSubduingPalm.java

---
+++ src/main/java/basicmod/cards/attack/AbstractEighteenDragonSubduingPalm.java
@@ -0,0 +1,69 @@
+package basicmod.cards.attack;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import basicmod.cards.BaseCard;
+import basicmod.util.CardStats;
+
+public abstract class AbstractEighteenDragonSubduingPalm extends BaseCard {
+
+    // 是否执行上一掌
+    protected boolean isRunPrePalm = false;
+
+    // 从1开始，所以小标要-1
+    protected int index = -1;
+
+    public AbstractEighteenDragonSubduingPalm(String ID, CardStats info, int index) {
+        super(ID, info);
+        this.index = index;
+    }
+
+    private static void runPrePlam(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster,
+        AbstractEighteenDragonSubduingPalm current) {
+        while (current.isRunPrePalm) {
+            AbstractEighteenDragonSubduingPalm preCard = current.getPreCard();
+            if (preCard == null) {
+                return;
+            }
+            if (current.upgraded) {
+                preCard.upgrade();
+            }
+            preCard.runPlam(abstractPlayer, abstractMonster);
+            current = preCard;
+        }
+    }
+
+    protected abstract void runPlam(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster);
+
+    private AbstractEighteenDragonSubduingPalm getNextCard() {
+        try {
+            return EighteenDragonSubduingPalmConstant.ABSTRACT_EIGHTEEN_DRAGON_SUBDUING_PALM_LIST.get(this.index);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private AbstractEighteenDragonSubduingPalm getPreCard() {
+        try {
+            return EighteenDragonSubduingPalmConstant.ABSTRACT_EIGHTEEN_DRAGON_SUBDUING_PALM_LIST.get(this.index - 2);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        AbstractEighteenDragonSubduingPalm nextCard = this.getNextCard();
+        if (nextCard != null) {
+            if (this.upgraded) {
+                nextCard.upgrade();
+            }
+            this.addToBot(new MakeTempCardInDiscardAction(nextCard, 1));
+        }
+        this.runPlam(abstractPlayer, abstractMonster);
+        AbstractEighteenDragonSubduingPalm current = this;
+        runPrePlam(abstractPlayer, abstractMonster, current);
+    }
+}
