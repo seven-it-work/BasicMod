016b33033fcc4c7efc634dabaa868b8e6e6bb299
seven <sevenyjl@gmail.com>
2024-12-31 23:09:41 +0800

    修复 九阳真经bug问题

--- src/main/java/basicmod/cards/skill/NineYangsScripture.java
+++ src/main/java/basicmod/cards/skill/NineYangsScripture.java
@@ -3,7 +3,7 @@ package basicmod.cards.skill;
 import basicmod.cards.BaseCard;
 import basicmod.cards.attack.EighteenDragonSubduingPalm1;
 import basicmod.util.CardStats;
-import cn.hutool.core.util.RandomUtil;
+import basicmod.util.GeneralUtils;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.colorless.DarkShackles;
@@ -14,17 +14,20 @@ import com.megacrit.cardcrawl.powers.watcher.BlockReturnPower;
 import com.megacrit.cardcrawl.powers.watcher.FreeAttackPower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NineYangsScripture extends BaseCard {
 
     public static final String ID = makeID(NineYangsScripture.class.getSimpleName());
 
     private static final CardStats info = new CardStats(CardColor.COLORLESS, CardType.SKILL, CardRarity.BASIC,
-            CardTarget.ENEMY, 1);
+            CardTarget.SELF, 1);
 
-    private static final List<Class> EFFECT = Arrays.asList(
+    private static final ArrayList<Class> EFFECT = new ArrayList<>(Arrays.asList(
             // 力量
             StrengthPower.class,
             // 下回合格挡
@@ -47,41 +50,41 @@ public class NineYangsScripture extends BaseCard {
             DoubleDamagePower.class,
             // 免费攻击
             FreeAttackPower.class
-    );
+    ));
 
 
     public NineYangsScripture() {
         super(ID, info);
-        setMagic(2, 3);
+        setMagic(2, 1);
         this.exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        RandomUtil.randomEleList(EFFECT, this.baseMagicNumber).forEach(aclass -> {
+        GeneralUtils.getRandomElementsUsingSet(EFFECT, this.baseMagicNumber).forEach(aclass -> {
             AbstractPower power = null;
             if (aclass.equals(StrengthPower.class)) {
-                power = new FrailPower(abstractMonster, 9, false);
+                power = new FrailPower(abstractPlayer, 9, false);
             } else if (aclass.equals(NextTurnBlockPower.class)) {
-                power = new NextTurnBlockPower(abstractMonster, 9);
+                power = new NextTurnBlockPower(abstractPlayer, 9);
             } else if (aclass.equals(PlatedArmorPower.class)) {
-                power = new PlatedArmorPower(abstractMonster, 9);
+                power = new PlatedArmorPower(abstractPlayer, 9);
             } else if (aclass.equals(DexterityPower.class)) {
-                power = new DexterityPower(abstractMonster, 9);
+                power = new DexterityPower(abstractPlayer, 9);
             } else if (aclass.equals(VigorPower.class)) {
-                power = new VigorPower(abstractMonster, 9);
+                power = new VigorPower(abstractPlayer, 9);
             } else if (aclass.equals(BlockReturnPower.class)) {
-                power = new BlockReturnPower(abstractMonster, 9);
+                power = new BlockReturnPower(abstractPlayer, 9);
             } else if (aclass.equals(ArtifactPower.class)) {
-                power = new ArtifactPower(abstractMonster, 9);
+                power = new ArtifactPower(abstractPlayer, 9);
             } else if (aclass.equals(BufferPower.class)) {
-                power = new BufferPower(abstractMonster, 9);
+                power = new BufferPower(abstractPlayer, 9);
             } else if (aclass.equals(ConservePower.class)) {
-                power = new ConservePower(abstractMonster, 9);
+                power = new ConservePower(abstractPlayer, 9);
             } else if (aclass.equals(DoubleDamagePower.class)) {
-                power = new DoubleDamagePower(abstractMonster, 9, false);
+                power = new DoubleDamagePower(abstractPlayer, 9, false);
             } else if (aclass.equals(FreeAttackPower.class)) {
-                power = new FreeAttackPower(abstractMonster, 9);
+                power = new FreeAttackPower(abstractPlayer, 9);
             } else {
                 // 不支持请继续添加
             }
--- src/main/java/basicmod/util/GeneralUtils.java
+++ src/main/java/basicmod/util/GeneralUtils.java
@@ -1,6 +1,37 @@
 package basicmod.util;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class GeneralUtils {
+
+    /**
+     * 从给定的列表中随机获取 num 个不重复的元素。
+     *
+     * @param list 输入的列表
+     * @param num  需要获取的元素数量
+     * @param <T>  列表中元素的类型
+     * @return 包含 num 个随机不重复元素的新列表
+     */
+    public static <T> List<T> getRandomElementsUsingSet(List<T> list, int num) {
+        if (num > list.size()) {
+            throw new IllegalArgumentException("不能从列表中选择比列表本身更多的元素");
+        }
+
+        Set<Integer> selectedIndices = new HashSet<>();
+        Random random = new Random();
+        while (selectedIndices.size() < num) {
+            selectedIndices.add(random.nextInt(list.size()));
+        }
+
+        return selectedIndices.stream()
+                .map(list::get)
+                .collect(Collectors.toList());
+    }
+
     public static String arrToString(Object[] arr) {
         if (arr == null)
             return null;
