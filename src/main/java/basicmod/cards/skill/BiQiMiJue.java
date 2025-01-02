ed159aad5c29a8101c67ed66f201e7bb03e26dbc
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2025-01-02 18:28:51 +0800

    Create BiQiMiJue.java

---
+++ src/main/java/basicmod/cards/skill/BiQiMiJue.java
@@ -0,0 +1,30 @@
+package basicmod.cards.skill;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.BufferPower;
+
+import basicmod.cards.BaseCard;
+import basicmod.powers.YunQiPower;
+import basicmod.util.CardStats;
+
+public class BiQiMiJue extends BaseCard {
+    public static final String ID = makeID(BiQiMiJue.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(CardColor.COLORLESS, CardType.SKILL, CardRarity.BASIC,
+        CardTarget.SELF, 2);
+
+    public BiQiMiJue() {
+        super(ID, info);
+        setMagic(3, 6);
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        this.addToBot(
+            new ApplyPowerAction(abstractPlayer, abstractPlayer, new YunQiPower(abstractPlayer, this.magicNumber),
+                this.magicNumber));
+        this.addToBot(new ApplyPowerAction(abstractPlayer, abstractPlayer, new BufferPower(abstractPlayer, 1), 1));
+    }
+}
