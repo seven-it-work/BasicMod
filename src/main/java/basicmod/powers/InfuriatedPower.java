fa49a5d87b30e26561a6c91a32253621a1723f98
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2025-01-02 18:28:03 +0800

    Create InfuriatedPower.java

---
+++ src/main/java/basicmod/powers/InfuriatedPower.java
@@ -0,0 +1,22 @@
+package basicmod.powers;
+
+import static basicmod.BasicMod.makeID;
+
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+
+public class InfuriatedPower extends BasePower {
+    public static final String ID = makeID(InfuriatedPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(ID);
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public InfuriatedPower(AbstractCreature owner, int amount) {
+        super(ID, PowerType.BUFF, false, owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0];
+    }
+}
