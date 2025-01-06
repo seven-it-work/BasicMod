1be3e2da9f1713989d6e0b2ec259ab6cf36d4c1d
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2025-01-06 16:46:43 +0800

    Create LuckyDraw1.java

---
+++ lottery/src/main/java/lottery/cards/status/LuckyDraw1.java
@@ -0,0 +1,18 @@
+package lottery.cards.status;
+
+import lottery.LotteryMod;
+
+import org.seven.util.CardStats;
+
+public class LuckyDraw1 extends BaseDraw {
+    public static final String ID = LotteryMod.resourcePath.makeID(LuckyDraw1.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(CardColor.COLORLESS, CardType.STATUS, CardRarity.CURSE,
+        CardTarget.SELF, 0);
+
+    public LuckyDraw1() {
+        super(ID, info,
+            BaseDraw.Probability.builder().basic(30).common(30).uncommon(15).rare(15).special(5).curse(5).build());
+        setMagic(1, 1);
+    }
+}
