432eb187ed308500e11a590bfeba5f21afbe7ff6
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2025-01-07 18:50:59 +0800

    Update SanLianReward.java

--- lottery/src/main/java/lottery/cards/status/SanLianReward.java
+++ lottery/src/main/java/lottery/cards/status/SanLianReward.java
@@ -7,13 +7,17 @@ import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
-import basemod.devcommands.relic.RelicAdd;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONObject;
 import lottery.LotteryMod;
 import lottery.cards.BaseCard;
 import lottery.relics.SanLianRelic;
 
 import org.seven.util.CardStats;
+import org.seven.util.GeneralUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SanLianReward extends BaseCard {
     public static final String ID = LotteryMod.resourcePath.makeID(SanLianReward.class.getSimpleName());
@@ -27,11 +31,23 @@ public class SanLianReward extends BaseCard {
         super(ID, info);
     }
 
+    private static final int maxRepeatTimes = 200;
+
+    private int repeatTimes = 0;
+
     public SanLianReward(CardRarity cardRarity) {
         super(ID, info);
         this.rarity = cardRarity;
         this.exhaust = true;
-        setMagic(1,1);
+        setMagic(1, 1);
+    }
+
+    @Override
+    public void initializeDescription() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.set("rarity", " " + LotteryMod.getKeywordsTranslation(this.rarity.name()) + " ");
+        this.rawDescription = GeneralUtils.tiHuan(this.rawDescription, jsonObject);
+        super.initializeDescription();
     }
 
     @Override
@@ -45,27 +61,53 @@ public class SanLianReward extends BaseCard {
             // RelicLibrary 中获取
             switch (this.rarity) {
                 case RARE:
-                    addTempRelic(abstractPlayer, RandomUtil.randomEle(RelicLibrary.rareList).makeCopy());
+                    addTempRelic(abstractPlayer, randomRelic(RelicLibrary.rareList).makeCopy());
                     break;
                 case BASIC:
-                    addTempRelic(abstractPlayer, RandomUtil.randomEle(RelicLibrary.starterList).makeCopy());
+                    addTempRelic(abstractPlayer, randomRelic(RelicLibrary.starterList).makeCopy());
                     break;
                 case SPECIAL:
-                    addTempRelic(abstractPlayer, RandomUtil.randomEle(RelicLibrary.specialList).makeCopy());
+                    addTempRelic(abstractPlayer, randomRelic(RelicLibrary.specialList).makeCopy());
                     break;
                 case COMMON:
-                    addTempRelic(abstractPlayer, RandomUtil.randomEle(RelicLibrary.commonList).makeCopy());
+                    addTempRelic(abstractPlayer, randomRelic(RelicLibrary.commonList).makeCopy());
                     break;
                 case UNCOMMON:
-                    addTempRelic(abstractPlayer, RandomUtil.randomEle(RelicLibrary.uncommonList).makeCopy());
+                    addTempRelic(abstractPlayer, randomRelic(RelicLibrary.uncommonList).makeCopy());
                     break;
                 case CURSE:
                 default:
-                    addTempRelic(abstractPlayer, RandomUtil.randomEle(RelicLibrary.shopList).makeCopy());
+                    addTempRelic(abstractPlayer, randomRelic(RelicLibrary.shopList).makeCopy());
                     break;
             }
         }
         this.used = true;
+    }
+
+    private AbstractRelic randomRelic(List<AbstractRelic> baseList) {
+        repeatTimes++;
+        AbstractRelic abstractRelic = RandomUtil.randomEle(baseList);
+        List<String> hasRelic = AbstractDungeon.player.relics.stream()
+            .filter(temp -> temp.relicId.equals(abstractRelic.relicId))
+            .map(temp -> temp.relicId)
+            .collect(Collectors.toList());
+        if (hasRelic.isEmpty()) {
+            repeatTimes = 0;
+            return abstractRelic;
+        } else {
+            // 存在遗物 则通过1/已有相同遗物数量
+            if (RandomUtil.randomInt(hasRelic.size() + 1) == 0) {
+                repeatTimes = 0;
+                return abstractRelic;
+            } else {
+                if (repeatTimes > maxRepeatTimes) {
+                    repeatTimes = 0;
+                    return abstractRelic;
+                } else {
+                    return randomRelic(baseList);
+                }
+            }
+        }
     }
 
     private void addTempRelic(AbstractPlayer abstractPlayer, AbstractRelic abstractRelicCurse) {
@@ -76,5 +118,9 @@ public class SanLianReward extends BaseCard {
         AbstractRelic.relicPage = 0;
         AbstractDungeon.topPanel.adjustRelicHbs();
         this.addToTop(new RelicAboveCreatureAction(abstractPlayer, abstractPlayer.getRelic(SanLianRelic.ID)));
+        // abstractRelicCurse的开始回合效果重新触发
+        abstractRelicCurse.atPreBattle();
+        abstractRelicCurse.atBattleStartPreDraw();
+        abstractRelicCurse.atBattleStart();
     }
 }
